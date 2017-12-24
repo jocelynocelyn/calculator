387d72619e0fe5c609d19f3d96dea385474c71cb
Jocelyn Geiger <geigerjm312@gmail.com>
2017-12-24 14:47:36 -0500

    Ability to use clear button and push multiple numbers

--- script.js
+++ script.js
@@ -1,68 +1,113 @@
 const display = document.querySelector('#display');
+let displayValue = 0;
 
+function clearAll() {
+        displayValue = 0;
+        display.textContent = '';
+}
+
+let clearBtn = document.querySelector('.clearButton');
+clearBtn.addEventListener('click', clearAll)
+
+    let btn0 = document.querySelector('#btn0');
+btn0.addEventListener('click', () => {
+    if (displayValue == 0){
+        displayValue = "0";
+    } else {
+        displayValue = displayValue + "0";
+    }
+    display.textContent = displayValue;
+});
 
 let btn1 = document.querySelector('#btn1');
 btn1.addEventListener('click', () => {
-    let displayValue = 1;
+    if (displayValue == 0){
+        displayValue = "1";
+    } else {
+        displayValue = displayValue + "1";
+    }
         display.textContent = displayValue;
 });
 
 let btn2 = document.querySelector('#btn2');
 btn2.addEventListener('click', () => {
-    let displayValue = 2;
-display.textContent = displayValue;
+    if (displayValue == 0){
+        displayValue = "2";
+    } else {
+        displayValue = displayValue + "2";
+    }
+    display.textContent = displayValue;
 });
 
 let btn3 = document.querySelector('#btn3');
 btn3.addEventListener('click', () => {
-    let displayValue = 3;
-display.textContent = displayValue;
+    if (displayValue == 0){
+        displayValue = "3";
+    } else {
+        displayValue = displayValue + "3";
+    }
+    display.textContent = displayValue;
 });
 
 let btn4 = document.querySelector('#btn4');
 btn4.addEventListener('click', () => {
-    let displayValue = 4;
+    if (displayValue == 0){
+        displayValue = "4";
+    } else {
+        displayValue = displayValue + "4";
+    }
     display.textContent = displayValue;
 });
 
 let btn5 = document.querySelector('#btn5');
 btn5.addEventListener('click', () => {
-    let displayValue = 5;
+    if (displayValue == 0){
+        displayValue = "5";
+    } else {
+        displayValue = displayValue + "5";
+    }
     display.textContent = displayValue;
 });
 
 let btn6 = document.querySelector('#btn6');
 btn6.addEventListener('click', () => {
-    let displayValue = 6;
+    if (displayValue == 0){
+        displayValue = "6";
+    } else {
+        displayValue = displayValue + "6";
+    }
     display.textContent = displayValue;
 });
 
-let btn7 = document.querySelector('#btn7');
+let btn7= document.querySelector('#btn7');
 btn7.addEventListener('click', () => {
-    let displayValue = 7;
+    if (displayValue == 0){
+        displayValue = "7";
+    } else {
+        displayValue = displayValue + "7";
+    }
     display.textContent = displayValue;
 });
 
 let btn8 = document.querySelector('#btn8');
 btn8.addEventListener('click', () => {
-    let displayValue = 8;
+    if (displayValue == 0){
+        displayValue = "8";
+    } else {
+        displayValue = displayValue + "8";
+    }
     display.textContent = displayValue;
 });
 
 let btn9 = document.querySelector('#btn9');
 btn9.addEventListener('click', () => {
-    let displayValue = 9;
+    if (displayValue == 0){
+        displayValue = "9";
+    } else {
+        displayValue = displayValue + "9";
+    }
     display.textContent = displayValue;
 });
-
-let btn0 = document.querySelector('#btn0');
-btn0.addEventListener('click', () => {
-    let displayValue = 0;
-    display.textContent = displayValue;
-});
-
-
-
 function operate(num1, operation, num2) {
     if (operation = '+') {
         return add(num1, num2);
